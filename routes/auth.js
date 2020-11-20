222e1ba8d364f0b0afa9e5bdfab014fc9453277e
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2020-11-20 16:59:13 -0500

    fixed the bug related to deleting the . in emails

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ router.put(
     body('email').isEmail().withMessage('Please enter a valid email.').custom((value, { req }) => {
         return User.findOne({ email: value }).then(userDoc => {if (userDoc) {return Promise.reject('E-Mail address already exists!');}
         });
-      }).normalizeEmail(),
+      }),
     body('firstName').trim().not().isEmpty(),
     body('lastName').trim().not().isEmpty(),
     body('password').trim().isLength({ min: 6 })
